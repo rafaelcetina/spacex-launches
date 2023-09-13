b30a41acd5e306a4ec6d590444cedff87cf7269c
Rodolfo Rafael Dzul Cetina <rdzul@palaceresorts.com>
2023-09-13 17:00:02 -0600

    Update spacex.ts

--- src/services/spacex.ts
+++ src/services/spacex.ts
@@ -1,12 +1,11 @@
 import { type Doc, type SpaceXApiResponse } from '../types/api'
 
-// const BASE_URL = 'https://api.spacexdata.com/v5'
 const BASE_URL = import.meta.env.PUBLIC_BASE_URL
 
 export const getLaunchBy = async ({ id }: { id: string }) => {
   const res = await fetch(`${BASE_URL}/launches/${id}`)
-
   const { data: launch } = (await res.json()) as Launch
+
   return launch
 }
 
@@ -14,7 +13,7 @@ export const getLatestLaunches = async () => {
   const res = await fetch(`${BASE_URL}/launches`, {
     method: 'GET'
   })
-
   const { data: launches } = (await res.json()) as SpaceXApiResponse
+
   return launches
 }
