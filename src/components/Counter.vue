18fcb23b7972bcf42ce9ec5f4d82ab6a5710f12b
Rodolfo Rafael Dzul Cetina <rdzul@palaceresorts.com>
2023-09-13 12:43:18 -0600

    First commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const count = ref(0)
+const increment = () => count.value++
+const decrement = () => count.value--
+</script>
+<template>
+  <section class="mt-4">
+    <button class="border px-4 py-2 text-xl text-white" @click="decrement">
+      -
+    </button>
+    <span class="text-yellow-300 text-xl mx-4">{{ count }}</span>
+    <button class="border px-4 py-2 text-xl text-white" @click="increment">
+      +
+    </button>
+  </section>
+</template>
