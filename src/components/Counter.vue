1159d75c7cb34aab7cd7c8c64984086ea14a2834
Rodolfo Rafael Dzul Cetina <rdzul@palaceresorts.com>
2023-09-14 12:10:35 -0600

    SEO al 100

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 const count = ref(0)
 const increment = () => count.value++
 const decrement = () => count.value--
 </script>
 <template>
-  <section class="mt-4">
+  <div>
     <button class="border px-4 py-2 text-xl text-white" @click="decrement">
-      -
+      <span> - </span>
     </button>
     <span class="text-yellow-300 text-xl mx-4">{{ count }}</span>
     <button class="border px-4 py-2 text-xl text-white" @click="increment">
-      +
+      <span> + </span>
     </button>
-  </section>
+  </div>
 </template>
