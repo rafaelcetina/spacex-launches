e9cf21fefc82930c3bfdc5337cb10b981391a315
Rodolfo Rafael Dzul Cetina <rdzul@palaceresorts.com>
2023-09-14 10:52:14 -0600

    add dependencie

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
-
 import vue from '@astrojs/vue'
 
 // https://astro.build/config
